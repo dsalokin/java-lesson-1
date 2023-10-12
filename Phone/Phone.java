850b4226f78b35765307bae5d0b725fbea5bbba2
daniilsalokin <work.salokin@gmail.com>
2023-10-12 22:44:54 +0300

    add full contact list warning, change "null" on "...", show contact lost after any action, fix typos

--- Phone/Phone.java
+++ Phone/Phone.java
@@ -1,5 +1,6 @@
 package Phone;
 
+import java.sql.SQLOutput;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -8,13 +9,17 @@ public class Phone {
     public static void getContacts(String[] contactList) {
         System.out.println("Ваш список контактов");
         for (int i = 0; i < contactList.length; i++) {
-            System.out.printf("%d. | %s\n", i + 1, contactList[i]);
+            if (contactList[i] == null) {
+                System.out.printf("%d. | %s\n", i + 1, "...");
+            }else {
+                System.out.printf("%d. | %s\n", i + 1, contactList[i]);
+            }
+            }
         }
-    }
+
 
     public static void getContact(String[] contactList, int getContactId, String editContact) {
         if (getContactId < contactList.length && getContactId > 0) {
-            System.out.println("Введите телефон");
             contactList[getContactId - 1] = editContact;
         } else {
             System.out.println("Диапазон значений от 0 до 9");
@@ -25,9 +30,12 @@ public class Phone {
         for (int i = 0; i < contactList.length; i++) {
             if (contactList[i] == null) {
                 contactList[i] = newContact;
-                break;
+                return;
+            }else if (i == contactList.length-1){
+                System.out.println("Список контактов полный, необходимо удалить или отредактировать существующий контакт");
             }
         }
+
     }
 
     public static void main(String[] args) {
@@ -38,7 +46,7 @@ public class Phone {
         do {
             System.out.println("""
                     Введите цифру соответствующую пункту меню:
-                     1. Вывессти список контактоа
+                     1. Вывести список контактов
                      2. Записать контакт
                      3. Удалить контакт
                      4. Редактировать контакт
@@ -50,18 +58,21 @@ public class Phone {
                     getContacts(contactList);
                 }
                 case "2" -> {
-                    System.out.println("Ввведите телефон");
+                    System.out.println("Введите телефон");
                     addContact(contactList, sc.next());
+                    getContacts(contactList);
                 }
                 case "3" -> {
                     getContacts(contactList);
                     System.out.println("Введите id контакта для удаления");
                     getContact(contactList, sc.nextInt(), null);
+                    getContacts(contactList);
                 }
                 case "4" -> {
                     getContacts(contactList);
                     System.out.println("Введите id контакта для редактирования");
                     getContact(contactList, sc.nextInt(), sc.next());
+                    getContacts(contactList);
                 }
             }
         } while (!Objects.equals(initialAnswer, "5"));
